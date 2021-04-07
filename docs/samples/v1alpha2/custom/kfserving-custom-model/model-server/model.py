fbfe0eb6dcd02da5a187805946b4e8b7237c3f11
titoeb <timtoebrock@gmail.com>
2021-04-07 09:58:43 +0200

    Updated serving model

--- docs/samples/v1alpha2/custom/kfserving-custom-model/model-server/model.py
+++ docs/samples/v1alpha2/custom/kfserving-custom-model/model-server/model.py
@@ -24,37 +24,8 @@ class KFServingSampleModel(kfserving.KFModel):
         self.ready = True
 
     def predict(self, request: Dict) -> Dict:
-        inputs = request["instances"]
 
-        # Input follows the Tensorflow V1 HTTP API for binary values
-        # https://www.tensorflow.org/tfx/serving/api_rest#encoding_binary_values
-        data = inputs[0]["image"]["b64"]
-
-        raw_img_data = base64.b64decode(data)
-        input_image = Image.open(io.BytesIO(raw_img_data))
-
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
-
-        input_tensor = preprocess(input_image)
-        input_batch = input_tensor.unsqueeze(0)
-
-        output = self.model(input_batch)
-
-        scores = torch.nn.functional.softmax(output, dim=1)[0]
-
-        _, top_5 = torch.topk(output, 5)
-
-        results = {}
-        for idx in top_5[0]:
-            results[self.classes[idx]] = scores[idx].item()
-
-        return {"predictions": results}
+        return {"predictions": "hello world"}
 
 
 if __name__ == "__main__":
